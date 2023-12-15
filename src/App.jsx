481f2ee2d31d169b28923285158700b400a27ea4
adrianblazejczyk <121712995+adrianblazejczyk@users.noreply.github.com>
2023-12-15 02:11:33 +0100

    fix 2

--- src/App.jsx
+++ src/App.jsx
@@ -16,8 +16,8 @@ const App = () => {
     <Routes>
       <Route path="/" element={<SharedLayout />}>
         <Route index element={<Home />} />
-        <Route path="/movies" element={<Movies />} />
-        <Route path="/movies/:productId" element={<MovieDetails />}>
+        <Route path="movies" element={<Movies />} />
+        <Route path="movies/:productId" element={<MovieDetails />}>
           <Route path="cast" element={<Cast />} />
           <Route path="reviews" element={<Reviews />} />
         </Route>
